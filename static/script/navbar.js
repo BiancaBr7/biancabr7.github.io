c9f24b67d8c7ad890d8d6dfab7680210cd827931
Bianca Bai <110609439+BiancaBr7@users.noreply.github.com>
2024-01-19 18:26:56 -0500

    more changes

--- static/script/navbar.js
+++ static/script/navbar.js
@@ -1,4 +1,14 @@
+var open = false
 function toggleNavbar() {
+    var toggle = document.querySelector('.menu-toggle')
+    if (open){
+        toggle.textContent = "☰"
+        open = false
+    }
+    else{
+        toggle.textContent = "✖"
+        open = true
+    }
     var navList = document.querySelector('.navbar');
     navList.classList.toggle('show');
 }
